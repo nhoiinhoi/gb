c1b3ee12fd076d1b2b0632baed8327deb099fcfa
nhoiinhoi <88049861+nhoiinhoi@users.noreply.github.com>
2021-07-28 00:10:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Bồ à!',
+    introDesc: `thì là em iu anh rất nhìu
+    nên em hay suy nghĩ lung tung kiểu này kia
+    chứ em hông có ý gì đou ạ :(((
+    mong anh hỉu bé nhìu hơn 🥺,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Vì đã lở yêu Bảo nhìu chẳng dám hứa xa xôi 😙',
+    desc: 'Mong sao cho ta mãi bên nhau tới già ',
+    btnYes: 'Vẫn iu bé nhìu <33',
+    btnNo: 'Không iu bé nhìu nữa :( ',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'Gửi cho bé <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Bé biết mà 🥰. Yêu Bé nhiều nhiều 😘😘',
+    messDesc: 'Moaaaaa',
+    btnAccept: 'Iuu Phạm Gia Bảooo <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
